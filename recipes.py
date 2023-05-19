9449e35f7980eb25ea7dc37a574f8e00b0cab53b
rospi <62301694+venieris95@users.noreply.github.com>
2023-05-19 13:15:19 +0200

    Update recipes.py

--- recipes.py
+++ recipes.py
@@ -1,1 +1,21 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Recipe List",
+    page_icon="🍪",
+)
+
+st.title("Main Page")
+st.sidebar.title("Recipes")
+recipes = st.selectbox(
+...     'Select a recipe',
+...     ('Cookie', 'Brownie', 'Protein bar'))
+if recipes == "Cookie": 
+  recipe = Path('cookie.py').read_text()
+  st.write(recipe)
+if recipes == "Brownie": 
+  recipe = Path('brownie.txt').read_text()
+  st.write(recipe)
+if recipes == "Protein bar": 
+  recipe = Path('protbar.txt').read_text()
+  st.write(recipe)
